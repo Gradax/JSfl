0a1065e84c3eab9fc16dfc7702b2eb237edbf161
Gradax <knigsv89@gmail.com>
2020-10-28 10:16:36 +0200

    hw4 done!

--- 3 Task/gameCode.js
+++ 3 Task/gameCode.js
@@ -1,56 +1,10 @@
 var event, ok;
-var answers = [];
+var answers = 0;
 
-questionCycles(works.a00, works.a1, works.a2, works.a0); //Выводим первый вопрос
-answers.push(event);
-switch (event) {
-    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
-        questionCycles(works.b00, works.b1, works.b2, works.b0);
-        answers.push(event);
-        switch (event) {
-            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
-                questionCycles(works.d00, works.d1, works.d2, works.d0);
-                answers.push(event);
-                break;
-            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
-                questionCycles(works.d00, works.d1, works.d2, works.d0);
-                answers.push(event);
-                break;
-            case -1: // Второе действие
-                answers.push(event);
-                break;
-            default:
-                alert('Ошибка');
-        }
-        break;
-    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
-        questionCycles(works.c00, works.c1, works.c2, works.c0);
-        answers.push(event);
-        switch (event) {
-            case 1: // Второе действие
-                questionCycles(works.d00, works.d1, works.d2, works.d0);
-                answers.push(event);
-                break;
-            case 2: // Второе действие
-                questionCycles(works.d00, works.d1, works.d2, works.d0);
-                answers.push(event);
-                break;
-            case -1: // Второе действие
-                answers.push(event);
-                break;
-            default:
-                alert('Ошибка');
-        }
-        break;
-    case -1: // Первое действие
-        answers.push(event);
-        break;
-    default:
-        alert('Ошибка');
-}
-
-var questWind = parseInt(prompt("Какой номер Вашего ответа изволите лицезреть?"));
-alert("В " + questWind + " окне Вы нажали: " + answers[questWind - 1]);
+questionCycles(questions.a0, questions.a1, questions.a2, questions.a3, questions.a4, questions.a5);
+questionCycles(questions.b0, questions.b1, questions.b2, questions.b3, questions.b4, questions.b5);
+questionCycles(questions.c0, questions.c1, questions.c2, questions.c3, questions.c4, questions.c5);
+alert("Каким-то чудом вы ответили на " + answers + " вопросов.");
 alert('Спасибо за игру');
 
 //------------------------------------------
@@ -58,22 +12,27 @@ function isAnswer(q, event) {
     if (isNaN(event) || !isFinite(event)) {
         alert('Вы ввели недопустимый символ');
         return false;
-    } else if (event < 1 || event > q) {
+    } else if (event < 1 || event > 4) {
         alert('Ваше число выходит из допустимого диапозона');
         return false;
+    } else if (event != q) {
+        alert('Не угадали, подумайте еще раз! =)');
+        return false;
     }
+    alert('Верно!');
+    answers++;
     return true;
 
 }
 
-function questionCycles(xyz00, xyz1, xyz2, xyz0) {
+function questionCycles(xyz00, xyz1, xyz2, xyz3, xyz4, xyz5) {
     do {
         ok = false;
-        event = +prompt(xyz00 + xyz1 + xyz2 + '-1 - Выход из игры');
+        event = +prompt(xyz00 + "\n 1." + xyz1 + "\n 2." + xyz2 + "\n 3." + xyz3 + "\n 4." + xyz4 + '\n -1 - Выход из игры');
         if (event == -1) {
             break;
         } else {
-            ok = isAnswer(xyz0, event);
+            ok = isAnswer(xyz5, event);
         }
     } while (!ok);
 }
